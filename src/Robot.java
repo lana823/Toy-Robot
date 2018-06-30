6e65cdfe54ceac090987ab5f1fef497cb0143cf9
Lana Yang <lanayang@Lanas-MBP.lan>
2018-06-30 17:04:46 +1000

    add parameter

--- src/Robot.java
+++ src/Robot.java
@@ -1,4 +1,17 @@
+// this is the toy robot simulator 
+
 
 public class Robot {
+	
+	// this is four direction of robot facing
+	public enum FACING {NORTH, SOUTH, EAST, WEST;}
+	
+	private int x;
+
+	private int y;
+	
+	private FACING facing;
+
+
 
 }
