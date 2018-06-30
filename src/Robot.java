d7974aed9ac972176031c858d4a06a23876072c5
Lana Yang <lanayang@Lanas-MBP.lan>
2018-06-30 17:20:43 +1000

    add PLACE function

--- src/Robot.java
+++ src/Robot.java
@@ -4,13 +4,46 @@
 public class Robot {
 	
 	// this is four direction of robot facing
-	public enum FACING {NORTH, SOUTH, EAST, WEST;}
+	public enum FACING {NORTH, SOUTH, EAST, WEST}
 	
 	private int x;
 
 	private int y;
 	
 	private FACING facing;
+	
+	
+	public void place (int x, int y, String facing ) {
+
+		this.x = x;
+
+		this.y = y;
+
+		switch (Robot.FACING.valueOf(facing.toUpperCase())) {
+
+		case NORTH :
+			this.facing = FACING.EAST;
+			break;
+		
+		case SOUTH :
+			this.facing = FACING.WEST;
+			break;
+			
+		case EAST :
+			this.facing = FACING.SOUTH;
+			break;
+			
+		case WEST :
+			this.facing = FACING.NORTH;
+			break;
+			
+		default :
+			System.out.println("Please enter valid direction.");
+			break;
+
+		}
+
+	}
 
 
 
